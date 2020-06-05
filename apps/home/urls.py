08f9718c9911b58e42f9f25548e2d86e82164bfe
mohbadar <mohammadbadarhashimi@gmail.com>
2020-06-05 15:18:22 +0430

    mofa doc form

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.get_home, name="get_home"),
+    path('newform.py', views.get_newform_view, name="mohe_newform")
 
 
 	]
--- apps/home/views.py
+++ apps/home/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def get_home(request):
     return render(request, "home/index.html")
+
+def get_newform_view(request):
+    return render(request, "home/newform.html")
