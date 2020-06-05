bd8d35e10d4250f8d0c2f8d33d3f9e2b1abdb3bb
mohbadar <mohammadbadarhashimi@gmail.com>
2020-06-05 16:29:48 +0430

    instance model

--- apps/form/models.py
+++ apps/form/models.py
@@ -12,4 +12,15 @@ class Form(models.Model):
 
     def __str__(self):
         return self.name + " - " + self.code + " - "+ self.tenant.name
+
+
+class Instance (models.Model):
+    form = models.ForeignKey(to = Form, on_delete="cascade", parent_link=True, related_name="form_instance_relationship", to_field="id")
+    name = models.CharField(name="name", max_length=255, unique=True, db_index=True,help_text="Instance Name")
+    data = models.TextField(name="data", max_length=2500, unique=True, db_index=True,help_text="Data")
+    workflow_status = models.CharField(name="workflow_status", max_length=255, unique=True, db_index=True,help_text="Workflow Status")
+    tenant = models.ForeignKey(to= Tenant, on_delete= "cascade",to_field="id" )
+
+    def __str__(self):
+        return self.tenant.name + " - " + self.form.name + " - "+ self.name + " - "+ self.workflow_status
     
