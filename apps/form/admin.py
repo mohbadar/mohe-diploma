32dc82156f6cc88ad3a79f4b037d98e8b1287bfe
mohbadar <mohammadbadarhashimi@gmail.com>
2020-06-09 12:06:40 +0430

    form id generation

--- apps/form/admin.py
+++ apps/form/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Form
+from .models import Form, Instance
 # Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
 admin.site.register(Form)
+admin.site.register(Instance)
--- apps/form/models.py
+++ apps/form/models.py
@@ -3,6 +3,7 @@ from ..tenant.models import Tenant, Center
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.text import slugify
+import datetime
 # from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
@@ -28,14 +29,30 @@ class Form(models.Model):
     class Meta:
         ordering = ['-created_at']
 
-class Instance (models.Model):
+
+class Instance(models.Model):
     form = models.ForeignKey(to = Form, on_delete=models.CASCADE, parent_link=True, related_name="form_instance_relationship", to_field="id")
     name = models.CharField(name="name", max_length=255, unique=True, db_index=True,help_text="Instance Name")
     data = models.TextField(name="data", max_length=2500, unique=True, db_index=True,help_text="Data")
     workflow_status = models.CharField(name="workflow_status", max_length=255, unique=True, db_index=True,help_text="Workflow Status")
     tenant = models.ForeignKey(to= Tenant, on_delete= models.CASCADE,to_field="id" )
     center = models.ForeignKey(to=Center, on_delete=models.CASCADE, default=1,to_field="id")
+    instance_no = models.CharField(max_length=500, null=True, blank=True, editable=False)
+    
+    def save(self, *args, **kwargs):
+        last_instance = Instance.objects.all().order_by('id').last()
+        if not last_instance:
+            self.instance_no =  'ASR' + str(datetime.date.today().year) +"01"
+        else:
+            instance_no = last_instance.instance_no
+            instance_int = int(instance_no.split('ASR')[-1])
+            new_instance_int = instance_int + 1
+            new_instance_no = 'ASR' + str(new_instance_int)
+            self.instance_no = new_instance_no
+
+        super(Instance, self).save(*args, **kwargs) # Call the real save() method
+
 
     def __str__(self):
-        return self.tenant.name + " - " + self.form.name + " - "+ self.name + " - "+ self.workflow_status
-    
+        return self.tenant.name + " - " + self.form.name + " - "+ self.name + " - "+ self.workflow_status + " - "+ self.instance_no
+
