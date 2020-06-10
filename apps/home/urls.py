aadee330523c494dff41f9194b44e149b9dacae4
mohbadar <mohammadbadarhashimi@gmail.com>
2020-06-10 12:18:08 +0430

    nsc content

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.get_home, name="get_home"),
-    path('newform.py', views.get_newform_view, name="mohe_newform")
+    path('newform.py', views.get_newform_view, name="eform_newform"),
+    path(r'centers.py', views.get_centers_of_tenant, name="get_centers_of_tenant"),
 
 
 	]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from ..tenant.models import Tenant, Center , Template
+from django.http import JsonResponse
+
 # Create your views here.
 
 
@@ -6,4 +9,15 @@ def get_home(request):
     return render(request, "home/index.html")
 
 def get_newform_view(request):
-    return render(request, "home/newform.html")
+    tenants = Tenant.objects.all()
+    return render(request, "home/newform.html", {'tenants': tenants})
+
+
+def get_centers_of_tenant(request,code):
+    # tenantCode = request.GET.dict()['code']
+
+    centers = Center.objects.filter(tenant = Tenant.objects.filter(code=code).first())
+    # centers = list(Center.objects.all().values())
+    data = dict()
+    data['centers'] = centers
+    return JsonResponse(data)
